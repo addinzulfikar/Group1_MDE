7b8eff54afd98e87fc9b84d1adba4010c7fb661e
niikennayu <nikenayusalsabila2919@gmail.com>
2026-04-15 15:29:02 +0700

    feat(module-1): add warehouse and package API routes

--- routes/api.php
+++ routes/api.php
@@ -7,10 +7,15 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+// Modul 1: Warehouse Management
+use App\Http\Controllers\Api\WarehouseController;
+use App\Http\Controllers\Api\PackageController;
+
 // Modul 4: Fleet Management & Hub Monitoring
 use App\Http\Controllers\API\FleetController;
 use App\Http\Controllers\API\HubController;
 
+// Modul 4
 Route::prefix('fleet')->group(function () {
     Route::get('/', [FleetController::class, 'index']);
     Route::post('/', [FleetController::class, 'store']);
@@ -23,4 +28,22 @@ Route::prefix('fleet')->group(function () {
 Route::prefix('hub')->group(function () {
     Route::get('/', [HubController::class, 'index']);
     Route::get('/{id}/capacity', [HubController::class, 'checkCapacity']);
+});
+
+// Modul 1
+Route::prefix('warehouse')->group(function () {
+    Route::get('/', [WarehouseController::class, 'index']);
+    Route::post('/', [WarehouseController::class, 'store']);
+    Route::get('/{id}', [WarehouseController::class, 'show']);
+    Route::put('/{id}', [WarehouseController::class, 'update']);
+    Route::delete('/{id}', [WarehouseController::class, 'destroy']);
+});
+
+Route::prefix('package')->group(function () {
+    Route::get('/', [PackageController::class, 'index']);
+    Route::post('/register', [PackageController::class, 'store']);
+    Route::get('/{id}', [PackageController::class, 'show']);
+    Route::put('/{id}', [PackageController::class, 'update']);
+    Route::delete('/{id}', [PackageController::class, 'destroy']);
+    Route::get('/{id}/dimension', [PackageController::class, 'getDimension']);
 });
